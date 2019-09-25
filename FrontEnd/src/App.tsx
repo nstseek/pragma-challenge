e8e082ed67fd286689ec8801ec23b8c303dbe223
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-09-25 11:12:00 -0300

    First commit

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+    return (
+        <div className='app'>
+            <span>It is working!</span>
+        </div>
+    );
+};
+
+export default App;
