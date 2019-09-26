887a9fcc1073c401c763f13f53ff8d7c2ed4420d
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-09-26 11:27:47 -0300

    Remove js files

--- BackEnd/js/models/get-temp/getTemps.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var getTemperatures = function (req, res, beers) {
-    res.status(200);
-    res.json(beers);
-};
-exports["default"] = getTemperatures;
--- BackEnd/js/models/root.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var logStatus = function (req, res) {
-    res.status(200);
-    res.json({ message: 'Server listening' });
-};
-exports["default"] = logStatus;
